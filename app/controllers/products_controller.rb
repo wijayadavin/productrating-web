71013dbdd9fcb507465738d8f2e405287fa03f39
Hendrik Gani <hendrik.gani@gmail.com>
2020-06-05 18:35:34 +1000

    Implement product crud

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,37 @@
+class ProductsController < ApplicationController
+  before_action do
+    case action_name.to_sym
+    when :new, :create
+      @product = Product.new
+    when :show, :edit, :update, :destroy
+      @product = Product.find(params[:id])
+    end
+  end
+
+  def new
+  end
+
+  def create
+    @product.assign_attributes(product_params)
+    if @product.save
+      redirect_to products_url
+    else
+      flash[:error] = @product.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  # TODO: Implement: edit/update
+
+  def show
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :quantity, :price)
+  end
+end  
