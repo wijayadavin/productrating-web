e48bc35bb16aa9dc92c6a65c810ac7668810e5d1
Davin Wijaya <wijayadavin@gmail.com>
2021-01-06 17:03:41 +0700

    finished the whole purchase controller

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,36 +5,35 @@ class PurchasesController < ApplicationController
     case action_name.to_sym
     when :new, :create
       @purchase = @product.purchases.new
-    when :show, :destroy
+    when :show, :destroy, :update, :edit
       @purchase = @product.purchases.find(params[:id])
     end
   end
 
+
   def new
   end
 
+
   def create
-    # TODO: Also decrease product quantity.
+    # ✔️TODO: Also decrease product quantity.
     # - For example, if `purchase.quantity` is 3, decrease `product.quantity` by 3
     # - Display an error if `product.quantity` is less than 0 (negative number)
     @purchase.assign_attributes(purchase_params)
-    
-    # === UPDATE QUANTITY AFTER PURCHASE ===
+
     # Get product quantity (before purchase):
     @product_quantity_before = Product.find(params[:product_id])["quantity"].to_i
     @product_quantity_after = @product_quantity_before - purchase_params["quantity"].to_i
 
     if @product_quantity_after < 0
-      flash[:error] = 'Sorry, the purchase was failed due to product is currently Out of stock.'
-      redirect_to product_url(@product)
+      flash[:error] = '⚠️Sorry, the purchase was failed due to the product is currently out of stock.⚠️'
+      redirect_to product_url()
     end
 
     # If quantity after purchase is greater than 0, update `product.quantity`:
     if @purchase.save
-      puts "saving..."
-      puts @product_quantity_after
       @product.update_attributes({"quantity" => (@product_quantity_after)}) 
-      redirect_to product_url(@product)
+      redirect_to product_url()
 
     else
       flash[:error] = @purchase.errors.full_messages.join(', ')
@@ -42,26 +41,51 @@ class PurchasesController < ApplicationController
     end
   end
 
+
   def edit
-    # TODO: Show edit form
+    # ✔️TODO: Show edit form
   end
+
 
   def update
-    # TODO: Update record (save to database)
-  end
+    # ✔️TODO: Update record (save to database)
+    @new_record = params[:purchase]
 
-  def destroy
-    # TODO: Delete record
-      if @purchase.destroy
-        redirect_to product_url(@product)
+    # remove duplicated:
+    @new_record.each do |key, value|
+      if  @new_record[key].to_s == @purchase[key].to_s
+        @new_record.delete(key)
+      end
+    end
+
+    if @new_record.empty?
+      flash[:error] = '⚠️ Nothing changed ⚠️'
+      redirect_to product_purchase_url()
+    else
+      if @purchase.update({
+        "quantity" => @new_record["quantity"].to_i,
+        "delivery_address" => @new_record["delivery_address"].to_s
+        })
+        redirect_to product_purchase_url()
       else
         flash[:error] = @purchase.errors.full_messages.join(', ')
-        render :new
+      end
+    end
+  end
+
+
+  def destroy
+    # ✔️TODO: Delete record
+      if @purchase.destroy
+        redirect_to product_url(params["product_id"])
+      else
+        flash[:error] = @purchase.errors.full_messages.join(', ')
       end
   end
 
   def show
   end
+
 
   private
     def purchase_params
