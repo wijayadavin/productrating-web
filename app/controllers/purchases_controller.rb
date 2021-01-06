c2942e89f59e5e4ecc67b7b9312b0504d3b55c23
Davin Wijaya <wijayadavin@gmail.com>
2021-01-06 11:43:56 +0700

    out of stock feature on buy product

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,7 +5,7 @@ class PurchasesController < ApplicationController
     case action_name.to_sym
     when :new, :create
       @purchase = @product.purchases.new
-    when :show
+    when :show, :destroy
       @purchase = @product.purchases.find(params[:id])
     end
   end
@@ -19,7 +19,19 @@ class PurchasesController < ApplicationController
     # - Display an error if `product.quantity` is less than 0 (negative number)
     @purchase.assign_attributes(purchase_params)
     if @purchase.save
+      # === UPDATE QUANTITY AFTER PURCHASE ===
+      # Get product quantity (before purchase):
+      @product_quantity_before = Product.find(params[:product_id])["quantity"].to_i
+      @product_quantity_after = @product_quantity_before - purchase_params["quantity"].to_i
+
+      if @product_quantity_after < 0
+        flash[:error] = 'Sorry, the purchase was failed due to product is currently Out of stock.'
+        redirect_to product_url(@product)
+      else
+      # If quantity after purchase is greater than 0, update `product.quantity`:
+      @product.update_attributes({"quantity" => ()}) 
       redirect_to product_url(@product)
+      end
     else
       flash[:error] = @purchase.errors.full_messages.join(', ')
       render :new
@@ -36,6 +48,13 @@ class PurchasesController < ApplicationController
 
   def destroy
     # TODO: Delete record
+      puts "===== Deleting data purchase with ID = #{@purchase["id"]} ====="
+      if @purchase.destroy
+        redirect_to product_url(@product)
+      else
+        flash[:error] = @purchase.errors.full_messages.join(', ')
+        render :new
+      end
   end
 
   def show
