847e769242c277d6d15f87cf414aa54cd93d593a
Davin Wijaya <wijayadavin@gmail.com>
2021-01-07 13:18:51 +0700

    add review feature

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,21 +2,31 @@ class ReviewsController < ApplicationController
   before_action do
     case action_name.to_sym
     when :new, :create
-      @review = Review.new
+      @review = Review.new    
     end
   end
 
+
   def new
-    @review.assign_attributes(review_params)
   end
 
+
   def create
-    # TODO: Create the record in database
-    raise "TODO: Save to database: #{review_params}"
+    # ✔️TODO: Create the record in database
+    @review.assign_attributes(review_params)
+    @purchase = Purchase.find(params["review"][:purchase_id])
+
+    if @review.save
+      redirect_to product_purchase_url(@purchase["product_id"], @purchase["id"])
+    else
+      flash[:error] = @review.errors.full_messages.join(', ')
+      redirect_to product_purchase_url(@purchase["product_id"], @purchase["id"])
+    end
   end
+
 
   private
     def review_params
-      params.require(:review).permit(:purchase_id)
+      params.require(:review).permit(:purchase_id, :rating, :comment, :product_id)
     end
 end
